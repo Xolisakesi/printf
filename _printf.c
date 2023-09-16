5e1219e765d4c74e5c0318c02f5439ab93cd854a
Xolisa Kesi <xolisakesi@gmail.com>
2023-09-16 09:38:35 +0000

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _printf - function is used for formatted output 
+ * format: the format string that specifies the 
+ * format of the output. 
+ * Return: returns the number of characters printed 
+ * if successful
+ */
+int _printf(const char *format, ...)
+{
+
+}
